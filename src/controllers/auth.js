c8e11e48a76c4e1f92b238894c15f0e89fdc09f0
Jedeye <jody.septiawan5@gmail.com>
2022-06-16 09:25:04 +0700

    update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,88 @@
 // import model here
+const { user } = require('../../models')
 
 // import package here
+const joi = require('joi')
 
 exports.register = async (req, res) => {
-  // code here
+  // name, email, password
+
+  const schema = joi.object({
+    name: joi.string().min(3).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(4).required()
+  })
+
+  const { error } = schema.validate(req.body);
+
+  if(error){
+    return res.send({
+      error: error.details[0].message
+    })
+  }
+
+  try {
+  const data = await user.create(req.body)
+
+  res.send({
+    message: 'Register success',
+    data
+  })
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'Failed',
+      message: 'Server Error'
+    })
+  }
+
 };
 
 exports.login = async (req, res) => {
-  // code here
+
+  const schema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(4).required()
+  })
+
+  const { error } = schema.validate(req.body);
+
+  if(error){
+    return res.send({
+      error: error.details[0].message
+    })
+  }
+
+  try {
+
+  const {email, password} = req.body
+
+  const data = await user.findOne({
+    where: {
+      email
+    }
+  })
+
+  if(!data){
+    return res.send({
+      message: `Email: ${email} not found!`
+    })
+  }
+
+  if(password != data.password){
+    return res.send({
+      message: `Password not match!`
+    })
+  }
+
+  res.send({
+    message: 'Login success',
+  })
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'Failed',
+      message: 'Server Error'
+    })
+  }
 };
